cdb33bf35f3351b6960f6acb556bbed732b706eb
Tanishq Dhawan <tanishq897.be22@chitkara.edu.in>
2024-07-25 18:02:53 +0530

    airthmetic operations done

---
+++ 9_airthmeticoperations/airthmetic.java
@@ -0,0 +1,22 @@
+class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println(c); // addition
+
+        int d = a / b;
+        System.out.println(d); // division
+
+        int e = a * b;
+        System.out.println(e); // multiplication
+
+        int f = a - b;
+        System.out.println(f); // subtraction
+
+        int g = a % b;
+        System.out.println(g); // modulus
+
+        
+    }
+}
